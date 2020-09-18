fd383bf9a1bec61603a21ea6cbc240cbffd912cc
PanIntegralus <42538693+PanIntegralus@users.noreply.github.com>
2020-09-19 00:39:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ var bot = mineflayer.createBot({
     password: config.account.password,
     version: config.minecraft.version
 });
+
+
+bot.on("login", ()=>{
+    console.log(`Logged in as ${bot.username}\nServer IP: ${config.minecraft.serverIP}:${config.minecraft.serverPort}\n`)
+});
+
+
+bot.on("message", async message =>{
+    let chat = message.toString()
+    if (chat.includes("Friend request")) {
+        bot.chat(`/f accept`);
+    };
+    console.log(chat);
+});
